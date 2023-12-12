d6b61f5ddabf7796dce35b31003250237d3e29be
mathis <138501804+dev-sio-M@users.noreply.github.com>
2023-12-12 19:49:49 +0100

    Create Exercice-5.sql

---
+++ Exercice-5.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE FUNCTION getNomJour(date_param DATE) RETURNS VARCHAR AS $$
+DECLARE
+    nom_jour VARCHAR(20);
+BEGIN
+    nom_jour := CASE EXTRACT(DOW FROM date_param)
+        WHEN 0 THEN 'Dimanche'
+        WHEN 1 THEN 'Lundi'
+        WHEN 2 THEN 'Mardi'
+        WHEN 3 THEN 'Mercredi'
+        WHEN 4 THEN 'Jeudi'
+        WHEN 5 THEN 'Vendredi'
+        WHEN 6 THEN 'Samedi'
+        ELSE 'Jour inconnu'
+    END;
+
+    RETURN nom_jour;
+END;
+$$ LANGUAGE plpgsql;
