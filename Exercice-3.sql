39cce940d4ca4ebc7a53a0da678dd0e523ee4d3e
mathis <138501804+dev-sio-M@users.noreply.github.com>
2023-12-12 18:45:53 +0100

    Update Exercice-3.sql

--- Exercice-3.sql
+++ Exercice-3.sql
@@ -1,8 +1,8 @@
-CREATE OR REPLACE FUNCTION getNbJoursParMois(date_param DATE) RETURNS INT AS $$
+CREATE OR REPLACE FUNCTION getNbJoursParMois(date DATE) RETURNS INT AS $$
 DECLARE
     nb_jours INT;
 BEGIN
-    nb_jours := EXTRACT(DAY FROM (date_param + INTERVAL '1 MONTH' - INTERVAL '1 DAY'));
+    nb_jours := EXTRACT(DAY FROM (date + INTERVAL '1 MONTH' - INTERVAL '1 DAY'));
     RETURN nb_jours;
 END;
 $$ LANGUAGE plpgsql;
